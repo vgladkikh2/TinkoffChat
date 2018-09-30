27d2d6a859c82535f1c09d58eba648b6cbe215b6
me <me@macbook-air-me.dlink>
2018-09-30 22:45:26 +0300

    Improve visuals

--- TinkoffChat/ProfileViewController.swift
+++ TinkoffChat/ProfileViewController.swift
@@ -17,28 +17,26 @@ class ProfileViewController: UIViewController {
     @IBAction func cameraIconTapped(_ sender: Any) {
         print("Выбери изображение профиля")
     }
-    /*
-    override init(nibName nibNameOrNil: String?, bundle nibBundleOrNil: Bundle?) {
-        super.init(nibName: nibNameOrNil, bundle: nibBundleOrNil)
-        print("\(#function) -> \(editButton.frame)")
-    }
-    required init?(coder aDecoder: NSCoder) {
+    
+//    override init(nibName nibNameOrNil: String?, bundle nibBundleOrNil: Bundle?) { // is used when you create the view programmatically
+//        super.init(nibName: nibNameOrNil, bundle: nibBundleOrNil)
+//        print("\(#function) -> \(editButton.frame)")
+//    }
+    required init?(coder aDecoder: NSCoder) { // is used when the view is created from storyboard/xib
         super.init(coder: aDecoder)
+//        print("\(#function) -> \(editButton.frame)") // editButton здесь nil, так как кнопка еще не успела создаться (и не присвоился адрес в переменную editButton). Поэтому, естественно, падает с ошибкой в рантайме
     }
-    init() {
-        super.init(nibName: nil, bundle: nil)
-        print("\(#function) -> \(editButton.frame)")
-    }
-    */
+    
     override func viewDidLoad() {
         super.viewDidLoad()
         // Do any additional setup after loading the view, typically from a nib.
-        userPlaceholder.layer.cornerRadius = 25
+        let sizeCoefficient = self.view.frame.size.width / 375.0
+        userPlaceholder.layer.cornerRadius = 50 * sizeCoefficient
         userPlaceholder.layer.masksToBounds = true
-        cameraIcon.layer.cornerRadius = 25
+        cameraIcon.layer.cornerRadius = 50 * sizeCoefficient
         cameraIcon.layer.masksToBounds = true
-        editButton.layer.cornerRadius = 10
-        editButton.layer.borderWidth = 2
+        editButton.layer.cornerRadius = 15 * sizeCoefficient
+        editButton.layer.borderWidth = 2 * sizeCoefficient
         editButton.layer.borderColor = UIColor.black.cgColor
         print("\(#function) -> \(editButton.frame)")
     }
@@ -49,8 +47,7 @@ class ProfileViewController: UIViewController {
     
     override func viewDidAppear(_ animated:Bool) {
         super.viewDidAppear(animated)
-        print("\(#function) -> \(editButton.frame)")
-        // Так как в storyboard мы выбрали iphone SE, вью создавалось под его размеры, а после этого и перед его выводом оно перерисовалось под размеры того устройства, на котором сейчас запустилось.
+        print("\(#function) -> \(editButton.frame)") // Так как в storyboard мы выбрали iphone SE, вью создавалось под его размеры, а после этого и перед его выводом оно перерисовалось под размеры того устройства, на котором сейчас запустилось, а, так как размеры iphone SE и iphone X в поинтах отличаются, то и размеры и расположение фрейма editButton отличаются
     }
     
     override func viewWillLayoutSubviews() {
