ae00dc842ace0212286b047c040dffc9d2cb130d
me <me@macbook-air-me.dlink>
2018-10-01 00:54:49 +0300

    Added CustomImage

---
+++ TinkoffChat/CustomImage.swift
@@ -0,0 +1,26 @@
+//
+//  CustomImage.swift
+//  TinkoffChat
+//
+//  Created by me on 01/10/2018.
+//  Copyright © 2018 gladkikh. All rights reserved.
+//
+
+import UIKit
+
+@IBDesignable
+class CustomImage: UIImageView {
+    @IBInspectable var cornerRadius: CGFloat = 0 {
+        didSet {
+            self.layer.cornerRadius = cornerRadius
+        }
+    }
+    override func prepareForInterfaceBuilder() {
+        super.prepareForInterfaceBuilder()
+        self.layer.cornerRadius = cornerRadius
+    }
+    override func awakeFromNib() {
+        super.awakeFromNib()
+        self.layer.cornerRadius = cornerRadius
+    }
+}
