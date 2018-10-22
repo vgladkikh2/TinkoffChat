22b55650576c6ec2df22a6a74be63045a02e9716
me <me@macbook-air-me.dlink>
2018-10-22 06:47:52 +0300

    homework 5 - final

--- TinkoffChat/ProfileViewController.swift
+++ TinkoffChat/ProfileViewController.swift
@@ -368,7 +368,6 @@ class ProfileViewController: UIViewController, UIImagePickerControllerDelegate, 
     
     override func didReceiveMemoryWarning() {
         super.didReceiveMemoryWarning()
-        // Dispose of any resources that can be recreated.
     }
     
 }
