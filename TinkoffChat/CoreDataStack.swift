07ef61db733032f6838802ea6aa60e314ab88310
me <me@macbook-air-me.dlink>
2018-11-05 13:16:52 +0300

    lesson 7 - CoreDataStack continue

--- TinkoffChat/CoreDataStack.swift
+++ TinkoffChat/CoreDataStack.swift
@@ -70,5 +70,52 @@ class CoreDataStack {
         }
     }
     
+    func findOrInsertAppUser(in context: NSManagedObjectContext) -> AppUser? {
+        guard let model = context.persistentStoreCoordinator?.managedObjectModel else {
+            assert(false, "Model is not available in context!")
+            return nil
+        }
+        var appUser: AppUser?
+        guard let fetchRequest = AppUser.fetchRequestAppUser(model: model) else {
+            return nil
+        }
+        do {
+            let results = try context.fetch(fetchRequest)
+            assert(results.count < 2, "Multiple AppUsers found!")
+            if let foundUser = results.first {
+                appUser = foundUser
+            }
+        } catch {
+            print("failed to fetch AppUser: \(error)")
+        }
+        if appUser == nil {
+            appUser = AppUser.insertAppUser(in: context)
+        }
+        return appUser
+    }
 }
 
+extension User {
+    static func insertUser(in context: NSManagedObjectContext) -> User? {
+        guard let user = NSEntityDescription.insertNewObject(forEntityName: "User", into: context) as? User else { return nil }
+        return user
+    }
+}
+
+extension AppUser {
+    static func insertAppUser(in context: NSManagedObjectContext) -> AppUser? {
+        guard let appUser = NSEntityDescription.insertNewObject(forEntityName: "AppUser", into: context) as? AppUser else { return nil }
+        let currentUser = User.insertUser(in: context)
+        currentUser?.name = "aaa"
+        appUser.currentUser = currentUser
+        return appUser
+    }
+    static func fetchRequestAppUser(model: NSManagedObjectModel) -> NSFetchRequest<AppUser>? {
+        let templateName = "AppUser"
+        guard let fetchRequest = model.fetchRequestTemplate(forName: templateName) as? NSFetchRequest<AppUser> else {
+            assert(false, "No template with name: \(templateName)")
+            return nil
+        }
+        return fetchRequest
+    }
+}
